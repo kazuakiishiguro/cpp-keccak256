0502ae749f2a152c8311ecc2be542cfae61d5517
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2021-11-10 21:03:49 +0900

    tmp

--- keccak256.h
+++ keccak256.h
@@ -15,31 +15,31 @@ static constexpr int ROUNDS = 24;
 // determined by the round index. The round constant is the second input to
 // the ι step mapping.
 static constexpr std::array<std::uint64_t, ROUNDS> RNDC {
-     0x0000000000000001ULL, 0x0000000000008082ULL, 0x800000000000808aULL,
-     0x8000000080008000ULL, 0x000000000000808bULL, 0x0000000080000001ULL,
-     0x8000000080008081ULL, 0x8000000000008009ULL, 0x000000000000008aULL,
-     0x0000000000000088ULL, 0x0000000080008009ULL, 0x000000008000000aULL,
-     0x000000008000808bULL, 0x800000000000008bULL, 0x8000000000008089ULL,
-     0x8000000000008003ULL, 0x8000000000008002ULL, 0x8000000000000080ULL,
-     0x000000000000800aULL, 0x800000008000000aULL, 0x8000000080008081ULL,
-     0x8000000000008080ULL, 0x0000000080000001ULL, 0x8000000080008008ULL
+  0x0000000000000001ULL, 0x0000000000008082ULL, 0x800000000000808aULL,
+  0x8000000080008000ULL, 0x000000000000808bULL, 0x0000000080000001ULL,
+  0x8000000080008081ULL, 0x8000000000008009ULL, 0x000000000000008aULL,
+  0x0000000000000088ULL, 0x0000000080008009ULL, 0x000000008000000aULL,
+  0x000000008000808bULL, 0x800000000000008bULL, 0x8000000000008089ULL,
+  0x8000000000008003ULL, 0x8000000000008002ULL, 0x8000000000000080ULL,
+  0x000000000000800aULL, 0x800000008000000aULL, 0x8000000080008081ULL,
+  0x8000000000008080ULL, 0x0000000080000001ULL, 0x8000000080008008ULL
 };
 
 // Keccack-f[b] transform. b = 25w = 1600
 void KeccakF(uint64_t st[25]);
 
 class Keccak {
-  private:
-    union {
-        uint8_t b[200]; // The width of a KECCAK-p permutation in bits
-        uint64_t w[25]; // The lane of 64-bit 5 * 5 = 25 words. Also b = 25w = 1600
-    } st; // state
+ private:
+  union {
+    uint8_t b[200]; // The width of a KECCAK-p permutation in bits
+    uint64_t w[25]; // The lane of 64-bit 5 * 5 = 25 words. Also b = 25w = 1600
+  } st; // state
 
-    int pt, rsiz, mdlen; // mdlen = hash output in bytes
-  public:
-    Keccak(const void *in, size_t in_len, void *md, int _mdlen);
-    int Init(int _mdlen);
-    int Update(const void *data, size_t len);
-    int Finalize(void *md);
-    int Reset();
+  int pt, rsiz, mdlen; // mdlen = hash output in bytes
+ public:
+  Keccak(const void *in, size_t in_len, void *md, int _mdlen);
+  int Init(int _mdlen);
+  int Update(const void *data, size_t len);
+  int Finalize(void *md);
+  int Reset();
 };
--- main.cc
+++ main.cc
@@ -6,60 +6,60 @@
 
 // read a hex string, return byte length or -a on error.
 static int test_hexdigit(char c) {
-    if (c >= '0' && c <= '9')
-        return c - '0';
-    if (c >= 'A' && c <= 'F')
-        return c - 'A' + 10;
-    if (c >= 'a' && c <= 'f')
-        return c - 'a' + 10;
-    return -1;
+  if (c >= '0' && c <= '9')
+    return c - '0';
+  if (c >= 'A' && c <= 'F')
+    return c - 'A' + 10;
+  if (c >= 'a' && c <= 'f')
+    return c - 'a' + 10;
+  return -1;
 }
 
 static int test_readhex(uint8_t *buf, const char *str, int maxbytes) {
-    int i, h, l;
+  int i, h, l;
 
-    for (i = 0; i < maxbytes; ++i) {
-        h = test_hexdigit(str[2 * i]);
-        if (h < 0)
-            return i;
-        l = test_hexdigit(str[2 * i + 1]);
-        if (l < 0)
-            return i;
-        buf[i] = (h << 4) + l;
-    }
+  for (i = 0; i < maxbytes; ++i) {
+    h = test_hexdigit(str[2 * i]);
+    if (h < 0)
+      return i;
+    l = test_hexdigit(str[2 * i + 1]);
+    if (l < 0)
+      return i;
+    buf[i] = (h << 4) + l;
+  }
 
-    return i;
+  return i;
 }
 
 int main(int argc, char **argv) {
 
-    const char *testvec[][2] = {
-        {
-            "9F2FCC7C90DE090D6B87CD7E9718C1EA6CB21118FC2D5DE9F97E5DB6AC1E9C10",
-            "2F1A5F7159E34EA19CDDC70EBF9B81F1A66DB40615D7EAD3CC1F1B954D82A3AF"
-        }
-    };
-
-    uint8_t sha[64], buf[64], msg[256];
-
-    memset(sha, 0, sizeof(sha));
-    memset(buf, 0, sizeof(buf));
-    memset(msg, 0, sizeof(msg));
-
-    int msg_len = test_readhex(msg, testvec[0][0], sizeof(msg)); // 32
-    int sha_len = test_readhex(sha, testvec[0][1], sizeof(sha)); // 32
-
-    Keccak k(msg, msg_len, buf, sha_len);
-
-    int fails = 0;
-
-    if (memcmp(sha, buf, sha_len) != 0) {
-        fails++;
+  const char *testvec[][2] = {
+    {
+      "9F2FCC7C90DE090D6B87CD7E9718C1EA6CB21118FC2D5DE9F97E5DB6AC1E9C10",
+      "2F1A5F7159E34EA19CDDC70EBF9B81F1A66DB40615D7EAD3CC1F1B954D82A3AF"
     }
+  };
 
-    if (fails == 0) {
-        std::cout << "ok" << std::endl;
-    }
+  uint8_t sha[64], buf[64], msg[256];
 
-    return 0;
+  memset(sha, 0, sizeof(sha));
+  memset(buf, 0, sizeof(buf));
+  memset(msg, 0, sizeof(msg));
+
+  int msg_len = test_readhex(msg, testvec[0][0], sizeof(msg)); // 32
+  int sha_len = test_readhex(sha, testvec[0][1], sizeof(sha)); // 32
+
+  Keccak k(msg, msg_len, buf, sha_len);
+
+  int fails = 0;
+
+  if (memcmp(sha, buf, sha_len) != 0) {
+    fails++;
+  }
+
+  if (fails == 0) {
+    std::cout << "ok" << std::endl;
+  }
+
+  return 0;
 }
